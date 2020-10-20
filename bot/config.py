c537f8d0907ebac4fdf4112df68ccecf4454a382
AirForceSean <73171222+AirForceSean@users.noreply.github.com>
2020-10-20 18:08:07 +0700

    Add files via upload

---
+++ bot/config.py
@@ -0,0 +1,13 @@
+settings = {
+    'token': 'NzU3NjM5MzA2NzIxMzYyMTEx.X2jUlg.rnHlU0i3KPfhUtuBvfdKNj2xU8E',
+    'bot': 'Штурмотрон E-01',
+    'id': 757639306721362111,
+    'prefix': '%'
+}
+
+access = {
+    266846168779063296,
+    174432708456349696,
+    265401984381616128,
+    267250110012850176
+}
