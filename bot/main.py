19df245117e94553e894916871c4c40bdcad7292
AirForceSean <73171222+AirForceSean@users.noreply.github.com>
2020-10-20 19:13:43 +0700

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,21 +1,16 @@
 import os
 from discord.ext import commands
-from config import settings
-from config import access
 
-# import time
+bot = commands.Bot(command_prefix="!")
+TOKEN = os.getenv("DISCORD_TOKEN")
 
-bot = commands.Bot(command_prefix=settings['prefix'])
-
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user.name}({bot.user.id})")
 
 @bot.command()
-async def Kredo(ctx, number: int, *args):
-    if ctx.message.author.id in access:
-        if (number is None) or (len(args) == 0):
-            return await ctx.send('%Kredo (кол-во пингов) (Роли/пользователи, которых надо пингануть через пробел)')
-        for _ in range(number):
-            await ctx.send(" ".join(x for x in args))
-    else:
-        await ctx.send('**Только деду Хосту и владыке Kredo позволено пользоваться этой командой!** (*И моему '
-                       'создателю*)')
-bot.run(settings['token'])
+async def ping(ctx):
+    await ctx.send("pong")
+
+if __name__ == "__main__":
+    bot.run(TOKEN)
